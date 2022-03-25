dac6920e1b7590c59a46b8b3cab24a2b26941c5a
Dikran Teymur <dikran.teymur@appcent.mobi>
2022-03-25 18:19:00 +0300

    Revert "asdf"
    This reverts commit 37efdf85669fef144fecba3297234d6e1a1c0f3c.

--- SwiftUIBootcamp/TextControl.swift
+++ SwiftUIBootcamp/TextControl.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct TextControl: View {
     var body: some View {
-        VStack {
-            Text("text eklendi")
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
