4bd63e45b055782e8b2dcb36019866340f9ef00c
Dikran Teymur <dikran.teymur@appcent.mobi>
2022-03-25 18:18:11 +0300

    deneme

---
+++ SwiftUIBootcamp/TextControl.swift
@@ -0,0 +1,20 @@
+//
+//  TextControl.swift
+//  SwiftUIBootcamp
+//
+//  Created by Dikran Teymur on 25.03.2022.
+//
+
+import SwiftUI
+
+struct TextControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextControl_Previews: PreviewProvider {
+    static var previews: some View {
+        TextControl()
+    }
+}
